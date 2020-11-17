4f7b4a6cbf5cddea148e970b3deb3ab71b977e5f
Unshaded <ozcan.ali94@gmail.com>
2020-11-17 15:37:26 +0100

    integration class niveau et player

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Player import Player
+from Niveau import Niveau
 
 name_user = input("Bonjour je suis Python. Quel est votre pseudo ? ")
 player = Player()
@@ -16,7 +17,49 @@ regle="- Je viens de penser à un nombre entre 1 et 10. Devinez lequel ?\n\
 - Dès que vous devinez mon nombre : vous avez le droit de quitter le jeu et de partir avec vos gains OU de continuer le jeu en passant au level supérieur."
 print(regle)
 player.solde_depart=10
-mise=player.setSoldeAvecMise(5)
-print(type(mise))
-while mise:
-    print(mise)
+#faire appel fonction mise
+print("Le jeu commence, entrez votre mise : ? ")
+mise = player.setSoldeAvecMise
+
+nb_python = niveau.get_nb_python
+
+nb_user = int(input("Alors mon nombre est : ? " ))
+test = niveau.set_nb_user(nb_user)
+
+essaie = 1
+gain=10
+while essaie != 3 or nb_user!=nb_python:
+
+    if test:
+        nb_user = niveau.nb_user_true()
+
+        if nb_user > nb_python:
+            print("Votre nbre est trop grand !")
+            essaie+1
+            print("Il vous reste "+essaie - 3+" chance !")
+
+        if nb_user < nb_python:
+            print("Votre nbre est trop petit !")
+            essaie+1
+            print("Il vous reste "+essaie -3+" chance !")
+
+        if nb_user == nb_python:
+            print ("Bingo "+name_user+", vous avez gagné en "+essaie+" coup(s) et vous avez emporté "+gains+" € !")
+            break
+    else:
+        nb_user = int(input("Je ne comprends pas ! Entrer SVP un nombre entre 1 et 10 :  ?"))
+
+if nb_user != nb_python:
+    print("Vous avez perdu ! Mon nombre est "+nb_python+" !")
+
+continuer = input("Souhaitez-vous continuer la partie (O/N) ? ")
+
+while continuer != "O" or "o" or "oui" or "Oui" or "N" or "n" or "non" or "Non":
+    continuer=(input("Je ne comprends pas votre réponse. Souhaitez-vous continuer la partie (O/N) ?" ))
+
+if continuer == "O" or "o" or "oui" or "Oui":
+    print("Super !")
+
+if continuer == "N" or "n" or "non" or "Non":
+    print("Au revoir ! Vous finissez la partie avec "+gain+" €.")
+    quit
