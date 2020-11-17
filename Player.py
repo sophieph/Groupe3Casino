7374359345813bd302c1be708b02d0933df04a5a
sophieph <so.phouangsy@gmail.com>
2020-11-17 11:35:56 +0100

    player.py

---
+++ Player.py
@@ -0,0 +1,15 @@
+# Class User 
+
+class Player: # 
+    """Classe définissant un joueur caractérisée par :
+    - son nom
+    - son solde de départ
+    - son niveau maximum atteint pendant le jeu 
+    - son lieu de résidence"""
+
+    
+    def __init__(self, level): # Notre méthode constructeur
+        self.nom = ""
+        self.solde = ""
+        self.level = 1
+
