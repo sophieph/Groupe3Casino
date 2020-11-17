f05c59ab9b83ce7e551b6d0d883ce00085f228e6
Unshaded <ozcan.ali94@gmail.com>
2020-11-17 12:53:20 +0100

    debut

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-
+from Player import Player
 
 name_user = input("Bonjour je suis Python. Quel est votre pseudo ? ")
-print ("Hello "+name_user+", vous avez 10 €, Très bien ! Installez vous SVP à la table de pari.")
+player = Player()
+player.nom = name_user
+
+print ("Hello "+player.nom+", vous avez 10 €, Très bien ! Installez vous SVP à la table de pari.")
 regle="- Je viens de penser à un nombre entre 1 et 10. Devinez lequel ?\n\
 - Att : vous avez le droit à trois essais !\n\
 - Si vous devinez mon nombre dès le premier coup, vous gagnez le double de votre mise !\n\
@@ -12,10 +15,8 @@ regle="- Je viens de penser à un nombre entre 1 et 10. Devinez lequel ?\n\
     - de quitter le jeu.\n\
 - Dès que vous devinez mon nombre : vous avez le droit de quitter le jeu et de partir avec vos gains OU de continuer le jeu en passant au level supérieur."
 print(regle)
-mise = input("Le jeu commence, entrez votre mise : ?")
-while mise > 1:
-    mise = input("Le montant saisi n'est pas valide. Entrer SVP un montant entre 1 et 10 € :  ? ")
-    if mise == 0:
-        print("Aurevoir")
-        break
-
+player.solde_depart=10
+mise=player.setSoldeAvecMise(5)
+print(type(mise))
+while mise:
+    print(mise)
