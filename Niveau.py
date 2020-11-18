eee73a98217fb0a20899082cd78fe3e6a38cc64d
sophieph <so.phouangsy@gmail.com>
2020-11-18 11:30:13 +0100

    ok

--- Niveau.py
+++ Niveau.py
@@ -23,25 +23,32 @@ class Niveau:
         self.nb_python = n
 
     def nb_user_is_valid(self, nombre):
-        if (type(nombre) == int):
-            if (nombre >=0 and nombre <= (self.level*10)):
+        try:
+            nombre = int(nombre)
+            if (nombre > 0 and nombre <= (self.level*10)):
                 return True
             else:
                 return False
-        else :
+            
+        except ValueError:
             return False
 
     def nb_user_is_true(self, nb_user):
         return (self.nb_python == nb_user)
 
     def mise_is_valid(self, mise, solde):
-        if (type(mise) == int):
-            if (mise >=0 and mise <= solde):
-                return True
+        try:
+            mise = int(mise)
+            if (type(mise) == int):
+                if (mise > 0 and mise <= solde):
+                    return True
+                else:
+                    return False
             else:
                 return False
-        else:
+        except ValueError:
             return False
+
     def get_nb_coup_max(self, ):
         if(self.level == 1):
             return 3
@@ -49,6 +56,7 @@ class Niveau:
             return 5
         elif (self.level  == 3):
             return 7
+
     def get_gain(self, mise, nb_essai):
         if (nb_essai == 1):
             return 2*mise
@@ -56,6 +64,15 @@ class Niveau:
             return mise
         elif (nb_essai == 3):
             return mise/2
-    def update_nb_python(self, ):
+    def update_nb_python(self ):
         self.nb_python = random.randrange(1, 10*self.level)
 
+    # def is_int(self, nombre):
+    #     try:
+    #         nombre = int(nombre)
+    #         if (nombre > 0):
+    #             return nombre
+    #         else:
+    #             return False
+    #     except ValueError:
+    #         return False
--- main.py
+++ main.py
@@ -1,9 +1,101 @@
-print('Hello world')
-from random import randint
-from Niveau import Niveau 
 from Player import Player
+from Niveau import Niveau
+import time
 
-niveau = Niveau(1, 1)
-sophie = Player(niveau)
+level=Niveau(1)
+name_user = input("Bonjour je suis Python. Quel est votre pseudo ? ")
+player = Player(level)
+player.nom = name_user
 
-print(sophie.level.level)
+print ("Hello "+player.nom+", vous avez 10 €, Très bien ! Installez vous SVP à la table de pari.")
+regle="- Je viens de penser à un nombre entre 1 et 10. Devinez lequel ?\n\
+- Att : vous avez le droit à trois essais !\n\
+- Si vous devinez mon nombre dès le premier coup, vous gagnez le double de votre mise !\n\
+- Si vous le devinez au 2è coup, vous gagnez exactement votre mise !\n\
+- Si vous le devinez au 3è coup, vous gagnez la moitiè votre mise !\n\
+- Si vous ne le devinez pas au 3è coup, vous perdez votre mise et vous avez le droit :\n\
+    - de retenter votre chance avec l'argent qu'il vous reste pour reconquérir le level perdu.\n\
+    - de quitter le jeu.\n\
+- Dès que vous devinez mon nombre : vous avez le droit de quitter le jeu et de partir avec vos gains OU de continuer le jeu en passant au level supérieur."
+#print(regle)
+solde = player.solde
+
+jeu = True
+
+while jeu:
+    nb_python = level.get_nb_python()
+    print(nb_python)
+    nb_coup = level.get_nb_coup_max()
+
+    mise = input("Le jeu commence, entrez votre mise : ? ")
+    while ( not level.mise_is_valid(mise, solde)):
+        mise = input("Le montant saisi n'est pas valide. Entrer SVP un montant entre 1 et 10 € :  ? ")
+    
+    player.add_mise(mise)
+
+    nb_user = input("Alors mon nombre est : ? " )
+    essai = 1
+    print(essai)
+    time.sleep(1)
+
+    test = level.nb_user_is_valid(nb_user)
+
+    while essai < nb_coup or nb_user!=nb_python:
+
+        if test:
+            nb_user = level.nb_user_is_true(nb_user)
+
+            if nb_user > nb_python:
+                print("Votre nbre est trop grand !")
+                essai += 1
+                print(essai)
+                time.sleep(1)
+                reste = nb_coup - essai
+
+                print("Il vous reste "+  str(reste) +" chance !")
+
+            if nb_user < nb_python:
+                print("Votre nbre est trop petit !")
+                essai += 1
+                print(essai)
+                time.sleep(1)
+                reste = nb_coup - essai
+                print(type(reste))
+
+                print("Il vous reste "+ str(reste) +" chance !")
+
+            if nb_user == nb_python:
+                gain = level.get_gain(mise, essai)
+                print ("Bingo "+player.nom+", vous avez gagné en "+ str(essai) +" coup(s) et vous avez emporté "+ str(gain) +" € !")
+                break
+        else:
+            nb_user = input("Je ne comprends pas ! Entrer SVP un nombre entre 1 et 10 :  ?")
+            essai+=1
+
+
+    if nb_user != nb_python:
+        print("Vous avez perdu ! Mon nombre est "+nb_python+" !")
+        perdu = True
+
+    continuer = input("Souhaitez-vous continuer la partie (O/N) ? ")
+
+    while continuer != "O" or "o" or "N" or "n":
+        continuer = input("Je ne comprends pas votre réponse. Souhaitez-vous continuer la partie (O/N) ?" )
+
+    if continuer == "O" or "o" :
+        if perdu and level.get_level() != 1:
+            level=Niveau(level.get_level() - 1)
+            
+        
+        elif perdu and level.get_level() == 1:
+            level=Niveau(1)
+        
+        else:
+            level=Niveau(level.get_level() + 1)
+        
+        player.set_level(level)
+        
+    if continuer == "N" or "n" :
+        print("Au revoir ! Vous finissez la partie avec "+gain+" €.")
+        jeu = False
+        
