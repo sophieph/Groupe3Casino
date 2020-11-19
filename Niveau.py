b13f77db65aa5af98fb41187c1056944e25a059d
Unshaded <ozcan.ali94@gmail.com>
2020-11-19 14:47:58 +0100

    niveau definitif

--- Niveau.py
+++ Niveau.py
@@ -19,29 +19,36 @@ class Niveau:
     def set_level(self, l):
         self.level = l
 
-    def set_nb_python(self,n):
+    def set_nb_python(self, n):
         self.nb_python = n
 
     def nb_user_is_valid(self, nombre):
-        if (type(nombre) == int):
-            if (nombre >=0 and nombre <= (self.level*10)):
+        try:
+            nombre = int(nombre)
+            if (nombre > 0 and nombre <= (self.level*10)):
                 return True
             else:
                 return False
-        else :
+            
+        except ValueError:
             return False
 
     def nb_user_is_true(self, nb_user):
         return (self.nb_python == nb_user)
 
     def mise_is_valid(self, mise, solde):
-        if (type(mise) == int):
-            if (mise >=0 and mise <= solde):
-                return True
+        try:
+            mise = int(mise)
+            if (type(mise) == int):
+                if (mise > 0 and mise <= solde):
+                    return True
+                else:
+                    return False
             else:
                 return False
-        else:
+        except ValueError:
             return False
+
     def get_nb_coup_max(self):
         if(self.level == 1):
             return 3
@@ -49,7 +56,7 @@ class Niveau:
             return 5
         elif (self.level  == 3):
             return 7
-    
+
     # update the get_gain function 
     def get_gain(self, mise, nb_essai):
         if (self.level == 1):
@@ -73,7 +80,16 @@ class Niveau:
                 return mise
             elif (nb_essai == 5 or nb_essai == 6 or nb_essai == 7):
                 return mise/2
-
-    def update_nb_python(self):
+                
+    def update_nb_python(self ):
         self.nb_python = random.randrange(1, 10*self.level)
 
+    # def is_int(self, nombre):
+    #     try:
+    #         nombre = int(nombre)
+    #         if (nombre > 0):
+    #             return nombre
+    #         else:
+    #             return False
+    #     except ValueError:
+    #         return False
